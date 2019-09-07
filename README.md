04e331946950aae64dbb5d5b1db032ee3e7355ff
Zach Hammer <zachary_hammer@alumni.brown.edu>
2019-09-07 14:06:17 -0400

    Add to pypi (#9)
    * Add basic readme
    
    * Setup deploy scripts
    
    * Add py.typed
    
    * Add setup.py
    
    * Update README.md
    
    * Update readme
    
    * Update formatting

---
+++ README.md
@@ -0,0 +1,29 @@
+# Tornado Async Transformer
+
+A [libcst](https://github.com/Instagram/LibCST) transformer for updating tornado @gen.coroutine syntax to python3.5+ native async/await.
+
+[Check out the demo.](https://tornado-async-transformer.zhammer.now.sh/)
+
+### Usage
+You can either:
+- Add `tornado_async_transformer.TornadoAsyncTransformer` to your existing libcst codemod.
+- Or run `python -m tornado_async_transformer.tool my_project/` from the commandline.
+
+#### Example
+```diff
+ """
+ A simple coroutine.
+ """
+ from tornado import gen
+
+
+-@gen.coroutine
+-def call_api():
+-    response = yield fetch()
++async def call_api():
++    response = await fetch()
+     if response.status != 200:
+         raise BadStatusError()
+-    raise gen.Return(response.data)
++    return response.data
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name="tornado-async-transformer",
+    version="0.1.2",
+    description="libcst transformer and codemod for updating tornado @gen.coroutine syntax to python3.5+ native async/await",
+    url="https://github.com/zhammer/tornado-async-transformer",
+    packages=find_packages(exclude=["tests", "demo_site"]),
+    package_data={"tornado_async_transformer": ["py.typed"]},
+    install_requires=["libcst >= 0.1.2"],
+    author="Zach Hammer",
+    author_email="zachary_hammer@alumni.brown.edu",
+    license="MIT License",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
