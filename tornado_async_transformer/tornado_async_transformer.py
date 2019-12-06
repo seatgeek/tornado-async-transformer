31028002970f7165d52cd61de7cc442951e1c63c
Zach Hammer <zhammer@seatgeek.com>
2019-12-06 16:28:11 +0100

    Use more matchers!!

--- tornado_async_transformer/tornado_async_transformer.py
+++ tornado_async_transformer/tornado_async_transformer.py
@@ -10,6 +10,32 @@ from tornado_async_transformer.helpers import (
 )
 
 
+# matchers
+gen_return_statement_matcher = m.Raise(exc=some_version_of("tornado.gen.Return"))
+gen_return_call_with_args_matcher = m.Raise(
+    exc=m.Call(func=some_version_of("tornado.gen.Return"), args=[m.AtLeastN(n=1)])
+)
+gen_return_call_matcher = m.Raise(
+    exc=m.Call(func=some_version_of("tornado.gen.Return"))
+)
+gen_return_matcher = gen_return_statement_matcher | gen_return_call_matcher
+gen_sleep_matcher = m.Call(func=some_version_of("gen.sleep"))
+gen_task_matcher = m.Call(func=some_version_of("gen.Task"))
+gen_coroutine_decorator_matcher = m.Decorator(
+    decorator=some_version_of("tornado.gen.coroutine")
+)
+gen_test_coroutine_decorator = m.Decorator(
+    decorator=some_version_of("tornado.testing.gen_test")
+)
+coroutine_decorator_matcher = (
+    gen_coroutine_decorator_matcher | gen_test_coroutine_decorator
+)
+coroutine_matcher = m.FunctionDef(
+    asynchronous=None,
+    decorators=[m.ZeroOrMore(), coroutine_decorator_matcher, m.ZeroOrMore()],
+)
+
+
 class TransformError(Exception):
     """
     Error raised upon encountering a known error while attempting to transform
@@ -42,7 +68,7 @@ class TornadoAsyncTransformer(cst.CSTTransformer):
         return with_added_imports(updated_node, imports)
 
     def visit_Call(self, node: cst.Call) -> Optional[bool]:
-        if self.is_gen_task_call(node):
+        if m.matches(node, gen_task_matcher):
             raise TransformError(
                 "gen.Task (https://www.tornadoweb.org/en/branch2.4/gen.html#tornado.gen.Task) from tornado 2.4.1 is unsupported by this codemod. This file has not been modified. Manually update to supported syntax before running again."
             )
@@ -53,7 +79,7 @@ class TornadoAsyncTransformer(cst.CSTTransformer):
         if not self.in_coroutine(self.coroutine_stack):
             return updated_node
 
-        if self.is_gen_sleep_call(updated_node):
+        if m.matches(updated_node, gen_sleep_matcher):
             self.required_imports.add("asyncio")
             return updated_node.with_changes(
                 func=cst.Attribute(value=cst.Name("asyncio"), attr=cst.Name("sleep"))
@@ -62,7 +88,7 @@ class TornadoAsyncTransformer(cst.CSTTransformer):
         return updated_node
 
     def visit_FunctionDef(self, node: cst.FunctionDef) -> Optional[bool]:
-        self.coroutine_stack.append(self.is_coroutine(node))
+        self.coroutine_stack.append(m.matches(node, coroutine_matcher))
         # always continue to visit function
         return True
 
@@ -77,7 +103,7 @@ class TornadoAsyncTransformer(cst.CSTTransformer):
             decorators=[
                 decorator
                 for decorator in updated_node.decorators
-                if not self.is_gen_coroutine_decorator(decorator)
+                if not m.matches(decorator, gen_coroutine_decorator_matcher)
             ],
             asynchronous=cst.Asynchronous(),
         )
@@ -88,7 +114,7 @@ class TornadoAsyncTransformer(cst.CSTTransformer):
         if not self.in_coroutine(self.coroutine_stack):
             return updated_node
 
-        if not self.is_gen_return(updated_node):
+        if not m.matches(node, gen_return_matcher):
             return updated_node
 
         return_value, whitespace_after = self.pluck_gen_return_value(updated_node)
@@ -132,10 +158,6 @@ class TornadoAsyncTransformer(cst.CSTTransformer):
         )
 
     @staticmethod
-    def is_gen_sleep_call(node: cst.Call) -> bool:
-        return m.matches(node, m.Call(func=some_version_of("gen.sleep")))
-
-    @staticmethod
     def pluck_asyncio_gather_expression_from_yield_list_or_list_comp(
         node: cst.Yield,
     ) -> cst.BaseExpression:
@@ -143,10 +165,6 @@ class TornadoAsyncTransformer(cst.CSTTransformer):
             func=cst.Attribute(value=cst.Name("asyncio"), attr=cst.Name("gather")),
             args=[cst.Arg(value=node.value, star="*")],
         )
-
-    @staticmethod
-    def is_gen_task_call(node: cst.Call) -> bool:
-        return m.matches(node, m.Call(func=some_version_of("gen.Task")))
 
     @staticmethod
     def in_coroutine(coroutine_stack: List[bool]) -> bool:
@@ -159,59 +177,11 @@ class TornadoAsyncTransformer(cst.CSTTransformer):
     def pluck_gen_return_value(
         node: cst.Raise,
     ) -> Tuple[Optional[cst.BaseExpression], cst.SimpleWhitespace]:
-        if TornadoAsyncTransformer.is_gen_return_call(node) and len(node.exc.args):
+        if m.matches(node, gen_return_call_with_args_matcher):
             return node.exc.args[0].value, node.whitespace_after_raise
 
         # if there's no return value, we don't preserve whitespace after 'raise'
         return None, cst.SimpleWhitespace("")
-
-    @staticmethod
-    def is_gen_return(node: cst.Raise) -> bool:
-        return TornadoAsyncTransformer.is_gen_return_call(
-            node
-        ) or TornadoAsyncTransformer.is_gen_return_statement(node)
-
-    @staticmethod
-    def is_gen_return_statement(node: cst.Raise) -> bool:
-        return m.matches(node, m.Raise(exc=some_version_of("tornado.gen.Return")))
-
-    @staticmethod
-    def is_gen_return_call(node: cst.Raise) -> bool:
-        return m.matches(
-            node, m.Raise(exc=m.Call(func=some_version_of("tornado.gen.Return"))),
-        )
-
-    @staticmethod
-    def is_coroutine_decorator(decorator: cst.Decorator) -> bool:
-        return (
-            TornadoAsyncTransformer.is_gen_coroutine_decorator(decorator)
-            or TornadoAsyncTransformer.is_gen_test_coroutine_decorator(decorator)
-        )
-
-    @staticmethod
-    def is_gen_coroutine_decorator(node: cst.Decorator) -> bool:
-        return m.matches(
-            node, m.Decorator(decorator=some_version_of("tornado.gen.coroutine"))
-        )
-
-    @staticmethod
-    def is_gen_test_coroutine_decorator(node: cst.Decorator) -> bool:
-        return m.matches(
-            node, m.Decorator(decorator=some_version_of("tornado.testing.gen_test"))
-        )
-
-    @staticmethod
-    def is_coroutine(function_def: cst.FunctionDef) -> bool:
-        # function already uses async def
-        if not function_def.asynchronous is None:
-            return False
-
-        return any(
-            (
-                TornadoAsyncTransformer.is_coroutine_decorator(decorator)
-                for decorator in function_def.decorators
-            )
-        )
 
     @staticmethod
     def make_simple_package_import(package: str) -> cst.Import:
