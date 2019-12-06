1073f8c412fe6b9a93208ce495a77431e41d8c3c
Zach Hammer <zhammer@seatgeek.com>
2019-12-06 14:56:34 +0100

    First attempt at name_attr_possibiltiies (doesnt work)

--- tornado_async_transformer/helpers.py
+++ tornado_async_transformer/helpers.py
@@ -1,7 +1,8 @@
-from functools import singledispatch
+from functools import reduce, singledispatch
 from typing import List, Sequence, Union
 
 import libcst as cst
+from libcst import matchers as m
 
 
 def with_added_imports(
@@ -67,3 +68,50 @@ def attribute_matches(node: cst.Attribute, parts: Sequence[str]) -> bool:
         return False
 
     return name_or_attribute_matches(node.value, parts[:-1])
+
+
+def name_attr_possibilities(tag: str) -> List[Union[m.Name, m.Attribute]]:
+    """
+    Let's say we want to find all instances of coroutine decorators in our code. The torando coroutine
+    decorator can be imported and used in the following ways:
+
+    ```
+    import tornado; @tornado.gen.coroutine
+    from tornado import gen; @gen.coroutine
+    from tornado.gen import coroutine; @coroutine
+    ```
+
+    If we want to see if a decorator is a coroutine decorator, and we don't want the overhead of knowing
+    all of the module's imports, we can check if a decorator matches one of the following options to be
+    fairly confident it's a coroutine:
+    - tornado.gen.coroutine
+    - gen.coroutine
+    - coroutine
+
+    This doesn't account for renamed imports (since we're not import aware) but does a decent enough job.
+    Another option is to only match against the final Name in a Name, Attribute or nested Attribute, but
+    there doesn't seem to be a majorly simpler way to do that atm with libcst and this way we get some
+    extra protection from considering @this.is.not.a.coroutine a match for @tornado.gen.coroutine.
+
+    # We run this function on "tornado.gen.coroutine"
+    >>> tornado_gen_coroutine, gen_coroutine, coroutine = name_attr_possibilities("tornado.gen.coroutine")
+
+    # We have just the matcher Name "coroutine"
+    >>> coroutine
+    Name(value='coroutine',...)
+
+    # We have an attribute "gen.coroutine"
+    >>> gen_coroutine
+    Attribute(value=Name(value='gen',...), attr=Name(value='coroutine',...),...)
+
+    # We have a nested attribute "tornado.gen.coroutine"
+    >>> tornado_gen_coroutine
+    Attribute(value=Name(value='tornado',...), attr=Attribute(value=Name(value='gen',...), attr=Name(value='coroutine',...),...))
+    """
+    def reducer(accumulator: List[Union[m.Name, m.Attribute]], current: str) -> List[Union[m.Name, m.Attribute]]:
+        if not accumulator:
+            return [m.Name(current)]
+
+        return [m.Attribute(value=m.Name(current), attr=accumulator[0])] + accumulator
+
+    return reduce(reducer, reversed(tag.split(".")), [])
