71ff46b7e97d6854c6c5242d5e6a3b616e2f3631
Zach Hammer <zachary_hammer@alumni.brown.edu>
2019-12-06 16:36:14 +0100

    Use matchers! (#20)
    * First attempt at name_attr_possibiltiies (doesnt work)
    
    * Fixed it!
    
    * Use matchers for most matching
    
    * More using matchers
    
    * Use more matchers!!
    
    * Fix typo
    
    * Add setup.cfg
    
    * Remove unused import

--- tornado_async_transformer/helpers.py
+++ tornado_async_transformer/helpers.py
@@ -2,6 +2,7 @@ from functools import singledispatch
 from typing import List, Sequence, Union
 
 import libcst as cst
+from libcst import matchers as m
 
 
 def with_added_imports(
@@ -28,42 +29,72 @@ def with_added_imports(
 def _is_import_line(
     line: Union[cst.SimpleStatementLine, cst.BaseCompoundStatement]
 ) -> bool:
-    return (
-        isinstance(line, cst.SimpleStatementLine)
-        and len(line.body) == 1
-        and isinstance(line.body[0], (cst.Import, cst.ImportFrom))
-    )
+    return m.matches(line, m.SimpleStatementLine(body=[m.Import() | m.ImportFrom()]))
 
 
-def name_or_attribute_matches_one_of(
-    node: cst.BaseExpression, parts_options: Sequence[Sequence[str]]
-) -> bool:
-    return any(name_or_attribute_matches(node, parts) for parts in parts_options)
-
-
-@singledispatch
-def name_or_attribute_matches(node: cst.BaseExpression, parts: Sequence[str]) -> bool:
+def name_attr_possibilities(tag: str) -> List[Union[m.Name, m.Attribute]]:
     """
-    Returns true of a Name or Attribute node matches some list of parts, such
-    as "tornado.gen.coroutine." Returns False if node isn't a Name or Attribute.
+    Let's say we want to find all instances of coroutine decorators in our code. The torando coroutine
+    decorator can be imported and used in the following ways:
+
+    ```
+    import tornado; @tornado.gen.coroutine
+    from tornado import gen; @gen.coroutine
+    from tornado.gen import coroutine; @coroutine
+    ```
+
+    If we want to see if a decorator is a coroutine decorator, and we don't want the overhead of knowing
+    all of the module's imports, we can check if a decorator matches one of the following options to be
+    fairly confident it's a coroutine:
+    - tornado.gen.coroutine
+    - gen.coroutine
+    - coroutine
+
+    This doesn't account for renamed imports (since we're not import aware) but does a decent enough job.
+    Another option is to only match against the final Name in a Name, Attribute or nested Attribute, but
+    there doesn't seem to be a majorly simpler way to do that atm with libcst and this way we get some
+    extra protection from considering @this.is.not.a.coroutine a match for @tornado.gen.coroutine.
+
+    # We run this function on "tornado.gen.coroutine"
+    >>> tornado_gen_coroutine, gen_coroutine, coroutine = name_attr_possibilities("tornado.gen.coroutine")
+
+    # We have just the matcher Name "coroutine"
+    >>> coroutine
+    Name(value='coroutine',...)
+
+    # We have an attribute "gen.coroutine"
+    >>> gen_coroutine
+    Attribute(value=Name(value='gen',...), attr=Name(value='coroutine',...),...)
+
+    # We have a nested attribute "tornado.gen.coroutine"
+    >>> tornado_gen_coroutine
+    Attribute(value=Attribute(value=Name(value='tornado',...), attr=Name(value='gen',...),...), attr=Name(value='coroutine',...),...)
     """
-    return False
+
+    def _make_name_or_attribute(parts: List[str]) -> Union[m.Name, m.Attribute]:
+        if not parts:
+            raise RuntimeError("Expected a non empty list of strings")
+
+        # just a name, e.g. `coroutine`
+        if len(parts) == 1:
+            return m.Name(parts[0])
+
+        # a name and attribute, e.g. `gen.coroutine`
+        if len(parts) == 2:
+            return m.Attribute(value=m.Name(parts[0]), attr=m.Name(parts[1]))
+
+        # a complex attribute, e.g. `tornado.gen.coroutine`, we want to make
+        # the attribute with value `tornado.gen` and attr `coroutine`
+        value = _make_name_or_attribute(parts[:-1])
+        attr = _make_name_or_attribute(parts[-1:])
+        return m.Attribute(value=value, attr=attr)
+
+    parts = tag.split(".")
+    return [_make_name_or_attribute(parts[start:]) for start in range(len(parts))]
 
 
-@name_or_attribute_matches.register(cst.Name)
-def name_matches(node: cst.Name, parts: Sequence[str]) -> bool:
-    if not len(parts) == 1:
-        return False
-
-    return bool(node.value == parts[0])
-
-
-@name_or_attribute_matches.register(cst.Attribute)
-def attribute_matches(node: cst.Attribute, parts: Sequence[str]) -> bool:
-    if not len(parts) >= 2:
-        return False
-
-    if not name_or_attribute_matches(node.attr, parts[-1:]):
-        return False
-
-    return name_or_attribute_matches(node.value, parts[:-1])
+def some_version_of(tag: str) -> m.OneOf[m.Union[m.Name, m.Attribute]]:
+    """
+    Poorly named wrapper around name_attr_possibilities.
+    """
+    return m.OneOf(*name_attr_possibilities(tag))
